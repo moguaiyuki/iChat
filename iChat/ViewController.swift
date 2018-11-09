7b201b8d9827c709fd40584f3762e36953e09678
moguaiyuki <moguai.yuki0424@gmail.com>
2018-11-09 19:25:31 +0900

    Initial Commit

---
+++ iChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by 大塚悠貴 on 2018/11/09.
+//  Copyright © 2018年 otsuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
